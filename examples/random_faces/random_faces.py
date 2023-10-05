251a648eff35c0706f61a2ef60886ba5e8de2016
Petr Salnikov <petya.salnikov@gomobile.ru>
2023-10-05 12:46:07 +0300

    change random example

--- examples/random_faces/random_faces.py
+++ examples/random_faces/random_faces.py
@@ -3,10 +3,10 @@ import random
 import chernoff_face
 
 faces = []
-for i in range(16):  # generate 16 random faces
+for i in range(15):  # generate 15 random faces
     d = {}
     for key in chernoff_face.face_parameters:
         d[key] = random.random()
     face = chernoff_face.draw_face(d, caption=str(i + 1))
     faces.append(face)
-chernoff_face.draw_grid(faces, 2, 8, "random_faces.png")
+chernoff_face.draw_grid(faces, 3, 5, "random_faces.png")
