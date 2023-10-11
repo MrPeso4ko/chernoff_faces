3754d8a5af995a144fb83106d7ed97ca8f531399
Petr Salnikov <petya.salnikov@gomobile.ru>
2023-10-12 01:25:15 +0300

    fixed docstrings

--- chernoff_face/image/face.py
+++ chernoff_face/image/face.py
@@ -5,7 +5,11 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 class Face:
-    """Two-dimensional array of 1s and 0s, where 0 represent black pixel and 1 represent white pixel"""
+    """Object which represents drawn face:
+
+    It can be used as two-dimensional array of 1s and 0s, where 0 represent black pixel and 1 represent white pixel
+
+    Also contains information about face caption"""
     caption_font_size = 22
     x_low = -150
     x_high = 150
@@ -21,9 +25,9 @@ class Face:
 
         self.px = [[1] * (self.y_high - self.y_low) for _ in range(self.x_high - self.x_low)]
         self.caption = caption
-        p = os.path.dirname(__file__)
-        fontpath = os.path.abspath(os.path.join(p, "arial.ttf"))
-        self.font = ImageFont.truetype(fontpath, size=self.caption_font_size)
+        current_path = os.path.dirname(__file__)
+        font_path = os.path.abspath(os.path.join(current_path, "arial.ttf"))
+        self.font = ImageFont.truetype(font_path, size=self.caption_font_size)
         bbox = self.font.getbbox(self.caption)
         self.caption_size = bbox[2] - bbox[0], bbox[3] - bbox[1]
         self.ox, self.oy = -self.x_low, -self.y_low + self.caption_size[1]
@@ -38,7 +42,10 @@ class Face:
         """:returns: Coordinates of face's caption top left corner"""
         return self.ox - self.caption_size[0] // 2, 15
 
-    def to_image(self, filename="face"):
+    def to_image(self, filename="face.png"):
+        """Saves face as an image with filename *filename*.
+        Image type is guessed based on extension
+        :param filename: name of file"""
         img = Image.new("1", (self.x_high - self.x_low, self.y_high - self.y_low), "white")
         pixels = img.load()
         for x in range(self.x_high - self.x_low):
@@ -49,6 +56,10 @@ class Face:
         img.save(filename)
 
     def draw_on_image(self, coords: tuple[int, int], img: PIL.Image.Image):
+        """Draws a face on top of PIL Image.Image object
+
+        :param coords: tuple of coordinates, pointing where top left corner of the face should be positioned
+        :param img: PIL image object, on which face will be drawn"""
         pixels = img.load()
         for x in range(self.x_high - self.x_low):
             for y in range(self.y_high - self.y_low):
